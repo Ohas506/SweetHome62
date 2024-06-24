7299ceaf89a3e1b48d7ce8e93c6c4cf876f44499
Ohas506 <142551067+Ohas506@users.noreply.github.com>
2024-06-23 22:25:44 -0400

    -did some things

--- int.js
+++ int.js
@@ -3,12 +3,13 @@ campaignTrail_temp.election_json = [
         "model": "campaign_trail.election",
         "pk": 15,
         "fields": {
-            "year": 2000,
+            "year": 1988,
             "summary": "As Wallace lay defeated in 1958, broken by the southern machine that had done nothing to raise Alabama from its status as a \"Dead last State\", nothing shall stop him from taking power now.<br><br>While Wallace used to be a \"Racial Moderate\", favoring gradual emancipation since his time as a judge treating both blacks and whites fairly, the support of the NAACP to his campaign 4 years ago where no use faced with the candidate of the Klu Klux Klan.<br><br>Now, if he was to become anyone, to be able to do anything, he had to do everything to get to the governor's house.\n\nFor what was hell paved out of if but from good intention?",
             "image_url": "https://images-ext-1.discordapp.net/external/JS883FIYDLsQShLB8hLs_H8dyMyFXJ1dY48oM8x0UFk/https/i.imgur.com/WKRI1Mm.png?width=1280&height=720",
             "winning_electoral_vote_number": 270,
             "advisor_url": "https://images-ext-1.discordapp.net/external/KMwuxMDAp_8rRD8DjYnmqq3rKCJmOe51MasvlPNTynU/https/i.imgur.com/EoNXsm5.png?width=794&height=642",
             "recommended_reading": "<h4 style=\"margin-top: 0.5em;\">The Florida Recount / 2000 Election</h4><a href=\"http://www.amazon.com/exec/obidos/ASIN/B000RH0DVM/amhius20-20\" target=”_blank”>Jeffrey Toobin - Too Close to Call: The Thirty-Six-Day Battle to Decide the 2000 Election</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/B00EMLDHFI/amhius20-20\" target=”_blank”>Andrew E. Busch & James W. Ceaser - The Perfect Tie: The True Story of the 2000 Presidential Election</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/032110028X/amhius20-20\" target=”_blank”>Larry Sabato - Overtime! The Election 2000 Thriller</a><h4>Books by the Candidates</h4><a href=\"http://www.amazon.com/exec/obidos/ASIN/B000C4SV2I/amhius20-20\" target=”_blank”>George W. Bush - A Charge to Keep: My Journey to the White House</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/0452269350/amhius20-20\" target=”_blank”>Al Gore - Earth in the Balance</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/B003YFIYWY/amhius20-20\" target=”_blank”>The Ralph Nader Reader</a><h4>Links</h4><a href=\"http://www.digitalhistory.uh.edu/disp_textbook.cfm?smtID=2&psid=3377\" target=”_blank”>Digital History - The Disputed Election of 2000</a></br><a href=\"http://www.pbs.org/wnet/supremecourt/future/landmark_bush.html\" target=”_blank”><em>Bush v. Gore</em></a></br><a href=\"http://www.vanityfair.com/news/2004/10/florida-election-2000\" target=”_blank”>The Path to Florida</a></br><a href=\"http://www.historyplace.com/speeches/gore-concedes.htm\" target=”_blank”>Al Gore Concedes the 2000 Election</a>",
+            "recommended_reading": "<h4 style=\"margin-top: 0.5em;\">Books and Links</h4>Peter Goldman - <a href=\"http://www.amazon.com/exec/obidos/ASIN/B012HU7SZA/amhius20-20\" target=\"_blank\">The Quest for the Presidency, 1988</a></br>L.A. Times, November 10, 1988 - <a href=\"http://articles.latimes.com/1988-11-10/news/mn-299_1_dukakis-lost-reelection\" target=\"_blank\">How Presidential Race Was Won-and Lost : Michael S. Dukakis</a></br>L.A. Times, November 10, 1988 - <a href=\"http://articles.latimes.com/1988-11-10/news/mn-302_1_george-bush\" target=\"_blank\">How Presidential Race Was Won--and Lost : George Bush</a></br><a href=\"http://www.usnews.com/news/articles/2008/01/17/the-photo-op-that-tanked\" target=\"_blank\">Michael Dukakis: The Photo Op That Tanked</a>",
             "has_visits": 1,
             "no_electoral_majority_image": "https://www.jetsimon.com/public/static/images/2012-no-majority.jpg",
             "creator": "Dan Bryan",
@@ -43,7 +44,7 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 153,
+        "pk": 158,
         "fields": {
             "first_name": "Democratic party ",
             "last_name": "Of Alabama",
@@ -66,7 +67,7 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 154,
+        "pk": 153,
         "fields": {
             "first_name": "Ryan",
             "last_name": "deGraffenried",
@@ -89,7 +90,7 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 155,
+        "pk": 154,
         "fields": {
             "first_name": "Bull",
             "last_name": "Connor",
@@ -112,7 +113,7 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 156,
+        "pk": 155,
         "fields": {
             "first_name": "William",
             "last_name": "Longshore ",
@@ -135,7 +136,7 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 87,
+        "pk": 156,
         "fields": {
             "first_name": "",
             "last_name": "Independent",
@@ -158,112 +159,56 @@ campaignTrail_temp.candidate_json = [
     }
 ]
 
+
 campaignTrail_temp.running_mate_json = [
     {
         "model": "campaign_trail.running_mate",
         "pk": 110673,
         "fields": {
             "candidate": 152,
-            "running_mate": 153
+            "running_mate": 158
         }
     }
 ]
 
-campaignTrail_temp.credits = "Ohas and UlricMorningstarG"
+campaignTrail_temp.credits = "Ohas and Ul"
 
-campaignTrail_temp.global_parameter_json = [
-    {
-        "model": "campaign_trail.global_parameter",
-        "pk": 1,
-        "fields": {
-            "vote_variable": 1.125,
-            "max_swing": 0.12,
-            "start_point": 0.94,
-            "candidate_issue_weight": 10,
-            "running_mate_issue_weight": 3,
-            "issue_stance_1_max": -0.71,
-            "issue_stance_2_max": -0.3,
-            "issue_stance_3_max": -0.125,
-            "issue_stance_4_max": 0.125,
-            "issue_stance_5_max": 0.3,
-            "issue_stance_6_max": 0.71,
-            "global_variance": 0.01,
-            "state_variance": 0.005,
-            "question_count": 25,
-            "default_map_color_hex": "#C9C9C9",
-            "no_state_map_color_hex": "#999999"
-        }
-    }
-]
 
-campaignTrail_temp.opponents_default_json = [
-    {
-        "election": 9,
-        "candidates": [
-            77,
-            78,
-            79,
-            80,
-            81
-        ]
-    }
-]
-
-campaignTrail_temp.opponents_weighted_json = [
-    {
-        "election": 9,
-        "candidates": [
-            77,
-            78,
-            79,
-            80,
-            81
-        ]
-    }
-]
 
 campaignTrail_temp.temp_election_list = [
     {
         "id": 15,
-        "year": 2000,
+        "year": 1988,
         "is_premium": 0,
         "display_year": "1962 Sweet Home"
     }
 ]
 
-jet_data = {
-    "headerColor": "#a8c6fe",
-    "windowColor": "#a8c6fe",
-    "containerColor": "#f4a4c0",
-    "innerWindowColor": "#E8FBFF",
-    "bannerImageUrl": "https://i.imgur.com/EMGFKOc.jpeg",
-    "backgroundImageUrl": "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg",
-    "endingTextColor": "#000000",
-    "customQuote": "",
-    "gameTitle": "Sweet Home"
-}
-//#startcode
-            
-            
-            corrr = '\n              <h2>Sweet Home</h2>\n            ';
-            
-            
-            nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#565d8f";
 
-            nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#eef0f6";
-            
-            document.getElementsByClassName("game_header")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_title;
-            
-            $("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
-            
-            $(".container")[0].style.backgroundColor = "#393e60";
-            
-            document.getElementById("header").src = "https://i.imgur.com/EMGFKOc.jpeg";
-            
-            document.body.background = "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg";
-            
-            document.head.innerHTML += "<style>#results_container {color:#000000;} .inner_window_w_desc {background-color:#E8FBFF!important;}</style>";
-            
-            //#endcode
+
+//#startcode
+
+
+
+  
+corrr = '\n              <h2>Sweet Home</h2>\n            ';
             
             
+nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#565d8f";
+
+nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#eef0f6";
+
+document.getElementsByClassName("game_header")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_title;
+
+$("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
+
+$(".container")[0].style.backgroundColor = "#393e60";
+
+document.getElementById("header").src = "https://i.imgur.com/EMGFKOc.jpeg";
+
+document.body.background = "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg";
+
+document.head.innerHTML += "<style>#results_container {color:#000000;} .inner_window_w_desc {background-color:#E8FBFF!important;}</style>";
+
+//#endcode
+
