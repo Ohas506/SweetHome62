85749f3122426737d69cb3eb00a9a0640df4b467
Ohas506 <142551067+Ohas506@users.noreply.github.com>
2024-06-23 21:49:24 -0400

    -did a thing

--- int.js
+++ int.js
@@ -1,7 +1,7 @@
 campaignTrail_temp.election_json = [
     {
         "model": "campaign_trail.election",
-        "pk": 9,
+        "pk": 15,
         "fields": {
             "year": 2000,
             "summary": "As Wallace lay defeated in 1958, broken by the southern machine that had done nothing to raise Alabama from its status as a \"Dead last State\", nothing shall stop him from taking power now.<br><br>While Wallace used to be a \"Racial Moderate\", favoring gradual emancipation since his time as a judge treating both blacks and whites fairly, the support of the NAACP to his campaign 4 years ago where no use faced with the candidate of the Klu Klux Klan.<br><br>Now, if he was to become anyone, to be able to do anything, he had to do everything to get to the governor's house.\n\nFor what was hell paved out of if but from good intention?",
@@ -20,11 +20,11 @@ campaignTrail_temp.election_json = [
 campaignTrail_temp.candidate_json = [
     {
         "model": "campaign_trail.candidate",
-        "pk": 77,
+        "pk": 152,
         "fields": {
             "first_name": "George Corley ",
             "last_name": "Wallace",
-            "election": 9,
+            "election": 15,
             "party": "Alabama Democratic Party",
             "state": "Barbour",
             "priority": 1,
@@ -43,11 +43,11 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 81,
+        "pk": 153,
         "fields": {
             "first_name": "Democratic party ",
             "last_name": "Of Alabama",
-            "election": 9,
+            "election": 15,
             "party": "Democratic party",
             "state": "Nationwide",
             "priority": 1,
@@ -66,11 +66,11 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 78,
+        "pk": 154,
         "fields": {
             "first_name": "Ryan",
             "last_name": "deGraffenried",
-            "election": 9,
+            "election": 15,
             "party": "Alabama Democratic Party",
             "state": "Alabama",
             "priority": 1,
@@ -89,11 +89,11 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 79,
+        "pk": 155,
         "fields": {
             "first_name": "Bull",
             "last_name": "Connor",
-            "election": 9,
+            "election": 15,
             "party": "Alabama Democratic Party",
             "state": "Alabama",
             "priority": 1,
@@ -112,11 +112,11 @@ campaignTrail_temp.candidate_json = [
     },
     {
         "model": "campaign_trail.candidate",
-        "pk": 80,
+        "pk": 156,
         "fields": {
             "first_name": "William",
             "last_name": "Longshore ",
-            "election": 9,
+            "election": 15,
             "party": "Republican",
             "state": "Alabama",
             "priority": 1,
@@ -139,7 +139,7 @@ campaignTrail_temp.candidate_json = [
         "fields": {
             "first_name": "",
             "last_name": "Independent",
-            "election": 9,
+            "election": 15,
             "party": "Independent",
             "state": "Alabama",
             "priority": 1,
@@ -163,8 +163,8 @@ campaignTrail_temp.running_mate_json = [
         "model": "campaign_trail.running_mate",
         "pk": 110673,
         "fields": {
-            "candidate": 77,
-            "running_mate": 81
+            "candidate": 152,
+            "running_mate": 153
         }
     }
 ]
@@ -224,7 +224,7 @@ campaignTrail_temp.opponents_weighted_json = [
 
 campaignTrail_temp.temp_election_list = [
     {
-        "id": 9,
+        "id": 15,
         "year": 2000,
         "is_premium": 0,
         "display_year": "1962 Sweet Home"
@@ -266,271 +266,4 @@ jet_data = {
             
             //#endcode
             
-            campaignTrail_temp.election_json = [
-    {
-        "model": "campaign_trail.election",
-        "pk": 9,
-        "fields": {
-            "year": 2000,
-            "summary": "As Wallace lay defeated in 1958, broken by the southern machine that had done nothing to raise Alabama from its status as a \"Dead last State\", nothing shall stop him from taking power now.<br><br>While Wallace used to be a \"Racial Moderate\", favoring gradual emancipation since his time as a judge treating both blacks and whites fairly, the support of the NAACP to his campaign 4 years ago where no use faced with the candidate of the Klu Klux Klan.<br><br>Now, if he was to become anyone, to be able to do anything, he had to do everything to get to the governor's house.\n\nFor what was hell paved out of if but from good intention?",
-            "image_url": "https://images-ext-1.discordapp.net/external/JS883FIYDLsQShLB8hLs_H8dyMyFXJ1dY48oM8x0UFk/https/i.imgur.com/WKRI1Mm.png?width=1280&height=720",
-            "winning_electoral_vote_number": 270,
-            "advisor_url": "https://images-ext-1.discordapp.net/external/KMwuxMDAp_8rRD8DjYnmqq3rKCJmOe51MasvlPNTynU/https/i.imgur.com/EoNXsm5.png?width=794&height=642",
-            "recommended_reading": "<h4 style=\"margin-top: 0.5em;\">The Florida Recount / 2000 Election</h4><a href=\"http://www.amazon.com/exec/obidos/ASIN/B000RH0DVM/amhius20-20\" target=”_blank”>Jeffrey Toobin - Too Close to Call: The Thirty-Six-Day Battle to Decide the 2000 Election</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/B00EMLDHFI/amhius20-20\" target=”_blank”>Andrew E. Busch & James W. Ceaser - The Perfect Tie: The True Story of the 2000 Presidential Election</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/032110028X/amhius20-20\" target=”_blank”>Larry Sabato - Overtime! The Election 2000 Thriller</a><h4>Books by the Candidates</h4><a href=\"http://www.amazon.com/exec/obidos/ASIN/B000C4SV2I/amhius20-20\" target=”_blank”>George W. Bush - A Charge to Keep: My Journey to the White House</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/0452269350/amhius20-20\" target=”_blank”>Al Gore - Earth in the Balance</a></br><a href=\"http://www.amazon.com/exec/obidos/ASIN/B003YFIYWY/amhius20-20\" target=”_blank”>The Ralph Nader Reader</a><h4>Links</h4><a href=\"http://www.digitalhistory.uh.edu/disp_textbook.cfm?smtID=2&psid=3377\" target=”_blank”>Digital History - The Disputed Election of 2000</a></br><a href=\"http://www.pbs.org/wnet/supremecourt/future/landmark_bush.html\" target=”_blank”><em>Bush v. Gore</em></a></br><a href=\"http://www.vanityfair.com/news/2004/10/florida-election-2000\" target=”_blank”>The Path to Florida</a></br><a href=\"http://www.historyplace.com/speeches/gore-concedes.htm\" target=”_blank”>Al Gore Concedes the 2000 Election</a>",
-            "has_visits": 1,
-            "no_electoral_majority_image": "https://www.jetsimon.com/public/static/images/2012-no-majority.jpg",
-            "creator": "Dan Bryan",
-            "display_year": "1962 Sweet Home"
-        }
-    }
-]
-
-campaignTrail_temp.candidate_json = [
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 77,
-        "fields": {
-            "first_name": "George Corley ",
-            "last_name": "Wallace",
-            "election": 9,
-            "party": "Alabama Democratic Party",
-            "state": "Barbour",
-            "priority": 1,
-            "description": "There are few men that have had bigger burning ambition than George Wallace.<br><br> A judge by trade, from his time as delegate in 1948 to his first run in 1958, he had become both an anti establishment candidate in the one party state of the deep south as well as a populist. Having refused to support the Dixiecrat secession from the democratic convention against Truman and having gained the reputation as a judge which was both fair and Respectful to blacks, what was to stop him from gaining the governorship?<br><br>He had been beaten. Having spoken out against the Klan terrorist activities, he had no chance to succeed against hardline segregationist John M. Patterson. Four Years later, he was once again running for the governorship. When he talked about good roads and good schools and all those things that have been part of his career, nobody listened. When he shall talk about blacks, they will stomp the floor.",
-            "color_hex": "#0000FF",
-            "secondary_color_hex": null,
-            "is_active": 1,
-            "image_url": "https://images-ext-1.discordapp.net/external/GCOwlYFh1gDniMuO2YQLjwjevCJvegykUY9YPmtdTXg/https/i.imgur.com/MXwdV2s.png?width=388&height=518",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": null,
-            "candidate_score": 1,
-            "running_mate": false
-        }
-    },
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 89,
-        "fields": {
-            "first_name": "Democratic party ",
-            "last_name": "Of Alabama",
-            "election": 9,
-            "party": "Democratic party",
-            "state": "Nationwide",
-            "priority": 1,
-            "description": "<p>Put description here</p>",
-            "color_hex": "#0000FF",
-            "secondary_color_hex": null,
-            "is_active": 0,
-            "image_url": "https://images-ext-1.discordapp.net/external/DuvRliNnH9EvsCd4RdeLhskvWtrlCl6YMDfFIbTJGnQ/https/i.imgur.com/WpakIM8.png?width=956&height=1066",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": "To say that the democratic party of Alabama has nothing democratic about it would be stating plain facts.<br><br>Since the end of reconstruction and the near total disenfranchisement of all blacks in the state, the party has run functionally unopposed for nearly a century. Champion of the status quo, white interest and the continued domination of White on the segregated blacks, it was only with the walkout of 48 that things began to change.<br><br>The party has since become divided between progressivist, racial moderates and the hard line segregationist. No other party having any influence in the state, this has turned this party into a truly cutthroat institution where each and every member backstab and double cross their neighbor to gain power for themselves.<br><br>As a result of it, the perfectly dysfunctional institution is finally beginning to show cracks as the segregationist party took back control in 1958. If anything was to improve in the state so long seen as the least of the 50 states, decisive action needs to be taken.",
-            "candidate_score": 1,
-            "running_mate": true
-        }
-    },
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 78,
-        "fields": {
-            "first_name": "Ryan",
-            "last_name": "deGraffenried",
-            "election": 9,
-            "party": "Alabama Democratic Party",
-            "state": "Alabama",
-            "priority": 1,
-            "description": "<p>Put description here</p>",
-            "color_hex": "#0000FF",
-            "secondary_color_hex": null,
-            "is_active": 0,
-            "image_url": "https://images-ext-1.discordapp.net/external/ZLjroOGG977Z8Hn9FD7H_kYdfzbpFEd9fxer_ZeJTHo/https/i.imgur.com/pGUGbit.png?width=700&height=908",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": null,
-            "candidate_score": 1,
-            "running_mate": false
-        }
-    },
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 79,
-        "fields": {
-            "first_name": "Bull",
-            "last_name": "Connor",
-            "election": 9,
-            "party": "Alabama Democratic Party",
-            "state": "Alabama",
-            "priority": 1,
-            "description": "<p>Put description here</p>",
-            "color_hex": "#53d5fd",
-            "secondary_color_hex": null,
-            "is_active": 0,
-            "image_url": "https://images-ext-1.discordapp.net/external/a1V5C7J_Q_FA2A385c6NXh3a3dSk_nWuMjvf3FLufN0/https/i.imgur.com/MQbp2Xk.png?width=802&height=996",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": null,
-            "candidate_score": 1,
-            "running_mate": false
-        }
-    },
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 80,
-        "fields": {
-            "first_name": "William",
-            "last_name": "Longshore ",
-            "election": 9,
-            "party": "Republican",
-            "state": "Alabama",
-            "priority": 1,
-            "description": "<p>Put description here</p>",
-            "color_hex": "#ff2600",
-            "secondary_color_hex": null,
-            "is_active": 0,
-            "image_url": "https://images-ext-1.discordapp.net/external/frSf8gp2Qq-U_-a3iEJJkOsVPyzTE_NfiWPcQCrF6l8/https/i.imgur.com/SbW8Din.png?width=540&height=644",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": null,
-            "candidate_score": 1,
-            "running_mate": false
-        }
-    },
-    {
-        "model": "campaign_trail.candidate",
-        "pk": 81,
-        "fields": {
-            "first_name": "",
-            "last_name": "Independent",
-            "election": 9,
-            "party": "Independent",
-            "state": "Alabama",
-            "priority": 1,
-            "description": "<p>Put description here</p>",
-            "color_hex": "#00f900",
-            "secondary_color_hex": null,
-            "is_active": 0,
-            "image_url": "https://www.jetsimon.com/public/candidateexample.png",
-            "electoral_victory_message": "This guy wins!",
-            "electoral_loss_message": "This guy loses!",
-            "no_electoral_majority_message": "We all win?",
-            "description_as_running_mate": null,
-            "candidate_score": 1,
-            "running_mate": false
-        }
-    }
-]
-
-campaignTrail_temp.running_mate_json = [
-    {
-        "model": "campaign_trail.running_mate",
-        "pk": 110673,
-        "fields": {
-            "candidate": 77,
-            "running_mate": 89
-        }
-    }
-]
-
-campaignTrail_temp.credits = "Ohas and UlricMorningstarG"
-
-campaignTrail_temp.global_parameter_json = [
-    {
-        "model": "campaign_trail.global_parameter",
-        "pk": 1,
-        "fields": {
-            "vote_variable": 1.125,
-            "max_swing": 0.12,
-            "start_point": 0.94,
-            "candidate_issue_weight": 10,
-            "running_mate_issue_weight": 3,
-            "issue_stance_1_max": -0.71,
-            "issue_stance_2_max": -0.3,
-            "issue_stance_3_max": -0.125,
-            "issue_stance_4_max": 0.125,
-            "issue_stance_5_max": 0.3,
-            "issue_stance_6_max": 0.71,
-            "global_variance": 0.01,
-            "state_variance": 0.005,
-            "question_count": 25,
-            "default_map_color_hex": "#C9C9C9",
-            "no_state_map_color_hex": "#999999"
-        }
-    }
-]
-
-campaignTrail_temp.opponents_default_json = [
-    {
-        "election": 9,
-        "candidates": [
-            77,
-            78,
-            79,
-            80,
-            81
-        ]
-    }
-]
-
-campaignTrail_temp.opponents_weighted_json = [
-    {
-        "election": 9,
-        "candidates": [
-            77,
-            78,
-            79,
-            80,
-            81
-        ]
-    }
-]
-
-campaignTrail_temp.temp_election_list = [
-    {
-        "id": 9,
-        "year": 2000,
-        "is_premium": 0,
-        "display_year": "1962 Sweet Home"
-    }
-]
-
-jet_data = {
-                "headerColor": "#a8c6fe",
-                "windowColor": "#a8c6fe",
-                "containerColor": "#f4a4c0",
-                "innerWindowColor": "#E8FBFF",
-                "bannerImageUrl": "https://i.imgur.com/EMGFKOc.jpeg",
-                "backgroundImageUrl": "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg",
-                "endingTextColor": "#000000",
-                "customQuote": "\"I advocate hatred of no man, because hate will only compound the problems facing the South.\"",
-                "gameTitle": "Sweet Home"
-            }
-
-//#startcode
-
-
-corrr = '\n              <h2>Sweet Home</h2><font id="wittyquote" size="4" color="white"><em>"I advocate hatred of no man, because hate will only compound the problems facing the South."</em></font>\n            ';
-
-
-nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#565d8f";
-
-nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#eef0f6";
-
-document.getElementsByClassName("game_header")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_title;
-
-$("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
-
-$(".container")[0].style.backgroundColor = "#393e60";
-
-document.getElementById("header").src = "https://i.imgur.com/EMGFKOc.jpeg";
-
-document.body.background = "https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg";
-
-document.head.innerHTML += "<style>#results_container {color:#000000;} .inner_window_w_desc {background-color:#E8FBFF!important;}</style>";
-
-//#endcode
+            
